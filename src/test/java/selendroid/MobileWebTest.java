18f5d70aa91498e04a9f5642ac5a9ee8f4df72f1
huajie1 <limaohuaym@126.com>
2015-05-28 17:39:02 +0800

    提交selendroid的demo工程

---
+++ src/test/java/selendroid/MobileWebTest.java
@@ -0,0 +1,38 @@
+package selendroid;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.selendroid.client.SelendroidDriver;
+import io.selendroid.common.SelendroidCapabilities;
+
+public class MobileWebTest {
+	
+	public static void main(String[] args) throws InterruptedException{
+		DesiredCapabilities capa = SelendroidCapabilities.android();
+			WebDriver driver = null;
+			try{
+				driver = new SelendroidDriver(capa);
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+			
+			driver.get("http://www.testerhome.com");			
+			driver.findElement(By.xpath("//a[text()='登录']")).click();
+		    driver.findElement(By.xpath("//input[@id='user_login']")).clear();
+		    driver.findElement(By.xpath("//input[@id='user_login']")).sendKeys("luguo_");
+		    driver.findElement(By.xpath("//input[@id='user_password']")).clear();
+		    driver.findElement(By.xpath("//input[@id='user_password']")).sendKeys("limaohua071110");
+		    driver.findElement(By.name("commit")).click();
+            driver.findElement(By.className("dropdown")).click();//显示下拉框
+		    WebElement exit = driver.findElement(By.xpath("//a[text()='退出']"));
+		    exit.click();
+		    driver.quit();
+		  }
+			
+			
+	}
+
+
---
+++ src/test/java/selendroid/ToastTest.java
@@ -0,0 +1,31 @@
+package selendroid;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.selendroid.client.SelendroidDriver;
+import io.selendroid.common.SelendroidCapabilities;
+
+public class ToastTest {
+	public static void main(String[] args) throws Exception {
+		// System.out.println( "Hello World!" );
+		SelendroidCapabilities capa = new SelendroidCapabilities(
+				"io.selendroid.testapp:0.15.0");
+		WebDriver driver = new SelendroidDriver(capa);
+		// WebElement inputField = driver.findElement(By.id("my_text_field"));
+		WebElement displayToast = driver.findElement(By.xpath("//Button[@id='showToastButton']"));
+		displayToast.click();
+		waitForElement(By.partialLinkText("Hello selendroid toast!"), 3, driver);
+		driver.quit();
+	}
+
+	public static WebElement waitForElement(By by, int timeout, WebDriver driver) {
+		WebDriverWait wait = new WebDriverWait(driver, timeout);
+		WebElement element = wait.until(ExpectedConditions.presenceOfElementLocated(by));
+		return element;
+	}
+
+}
