11121d42312127f78146c60118f6f63a40274fb2
huajie1 <limaohuaym@126.com>
2015-06-08 16:09:01 +0800

    改用maven实现testerhome的登录退出，注释掉main函数实现方式

--- src/test/java/selendroid/MobileWebTest.java
+++ src/test/java/selendroid/MobileWebTest.java
@@ -1,5 +1,8 @@
 package selendroid;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -9,16 +12,17 @@ import io.selendroid.client.SelendroidDriver;
 import io.selendroid.common.SelendroidCapabilities;
 
 public class MobileWebTest {
+	WebDriver driver = null;
 
-	public static void main(String[] args) throws InterruptedException {
+	@Before
+	public void setUp() throws Exception {
 		DesiredCapabilities capa = SelendroidCapabilities.android();
-		WebDriver driver = null;
-		try {
-			driver = new SelendroidDriver(capa);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		driver = new SelendroidDriver(capa);
+	}
 
+	@Test
+	public void loginAndExit() throws Exception {
+		
 		driver.get("http://www.testerhome.com");
 		driver.findElement(By.xpath("//a[text()='登录']")).click();
 		driver.findElement(By.xpath("//input[@id='user_login']")).clear();
@@ -31,8 +35,35 @@ public class MobileWebTest {
 		driver.findElement(By.className("dropdown")).click();// 显示下拉框
 		WebElement exit = driver.findElement(By.xpath("//a[text()='退出']"));
 		exit.click();
-		driver.quit();
 	}
 
-}
+	@After
+	public void teardown() {
+		driver.quit();
+	}
+	
+	// public static void main(String[] args) throws InterruptedException {
+	// DesiredCapabilities capa = SelendroidCapabilities.android();
+	// WebDriver driver = null;
+	// try {
+	// driver = new SelendroidDriver(capa);
+	// } catch (Exception e) {
+	// e.printStackTrace();
+	// }
 
+	// driver.get("http://www.testerhome.com");
+	// driver.findElement(By.xpath("//a[text()='登录']")).click();
+	// driver.findElement(By.xpath("//input[@id='user_login']")).clear();
+	// driver.findElement(By.xpath("//input[@id='user_login']")).sendKeys(
+	// "luguo_");
+	// driver.findElement(By.xpath("//input[@id='user_password']")).clear();
+	// driver.findElement(By.xpath("//input[@id='user_password']")).sendKeys(
+	// "limaohua071110");
+	// driver.findElement(By.name("commit")).click();
+	// driver.findElement(By.className("dropdown")).click();// 显示下拉框
+	//WebElement exit = driver.findElement(By.xpath("//a[text()='退出']"));
+	//exit.click();
+	//driver.quit();
+	// }
+
+}
